3a08b39cb079e8d9cc658f713d5127ffa6285ca4
grantbi <grantbi@users.noreply.github.com>
2019-06-19 00:51:15 -0700

    Adding a tool to download and generate manifest.xml files.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Manifest Generator
+
+To install
+
+```
+npm install -g git:https://github.com/grantbi/manifest-generator.git
+```
+
+And to download/clone a manifest:
+```
+manifest-generator download https://join.cityofheroesrebirth.com/Manifests/Rebirth.xml v2i1
+```
+
+To generate a manifest based on that folder:
+```
+manifest-generator generate coh.my-server.com v2i1 v2i1
+```
---
+++ index.js
@@ -0,0 +1,211 @@
+var fs = require('fs'),
+    parser = require('fast-xml-parser'),
+    path = require('path'),
+    crypto = require("crypto");
+    request = require('request');
+
+// var exec = require("child_process").exec;
+var argv = require('yargs')
+    .usage('Usage: $0 <command> [options]')
+    .command({
+        aliases:['dl'],
+        command:"download [manifest] [folder-path]",
+        desc:"Download the manifest from a given url.",
+        builder:(yargs) => {
+            yargs.positional('manifest', { describe:'The manifest.xml url to download.', type:'string' })
+            .positional('folder-path', { describe:'The folder path to download to.', type:'string' })
+            .demandOption(['manifest', 'folder-path'], 'Please provide both run and path arguments to work with this tool')
+        }
+    })
+    .command({
+        aliases:['gen'],
+        command:"generate <domain> <from-folder> <versionLabel> [mirrors..]", 
+        desc:"Generates a new xml file based on the manifest using the domain given.",
+        builder: (yargs) => {
+            yargs.positional('domain', { describe:'The domain that will be hosting this manifest.xml file and its files.', type:'string' })
+            .positional('from-folder', { describe:'The folder to build the manifest from.', type:'string' })
+            .positional('versionLabel', { describe:'The version label to use.', type:'string' })
+            .positional('mirrors', { describe:'A list of domains to mirror from.', type:'string' })
+        }
+    })
+    .options({
+        /* 'run': {
+            alias: 'r',
+            describe: 'run your program',
+            demandOption: true
+        },
+        'path': {
+            alias: 'p',
+            describe: 'provide a path to file',
+            demandOption: true
+        },
+        'spec': {
+            alias: 's',
+            describe: 'program specifications'
+        } */
+    })
+    .demandCommand(2, "You need to specify at least two commands.")
+    .help()
+    .wrap(null)
+    .example('$0 download https://join.cityofheroesrebirth.com/Manifests/Rebirth.xml v2i1', 'Downloads the files from the manifest url and puts them into v2i1 folder.')
+    .example('$0 generate coh.my-server.com v2i1 v2i1 cityofhero.es/OuroDev brute.coh.network/OuroDev', 'Generates an example manifest with additional mirrors from v2i1 folder.')
+    .argv;
+
+// console.log('argv', argv)
+if (argv._[0] == 'dl' || argv._[0] == "download") {
+    console.log(`Downloading manifest from ${argv.manifest} to ${argv.folderPath}`)
+    try {fs.mkdirSync(`./${argv.folderPath}`, { recursive: true });} catch(e) {}
+    var download = function (uri, filename) {
+        return new Promise((resolve, rej) => {
+            request.head(uri, function (err, res, body) {
+                if (err) return rej(err)
+                // console.log('content-type:', res.headers['content-type']);
+                // console.log('content-length:', res.headers['content-length']);
+                request(uri).pipe(fs.createWriteStream(filename)).on('close', resolve);
+            });
+        })
+    };
+
+    (async () => {
+        let manifestFile = await download(argv.manifest, `./${argv.folderPath}/manifest.xml`)
+        let xml = fs.readFileSync(`./${argv.folderPath}/manifest.xml`, 'utf8')
+        let {manifest} = parser.parse(xml, {parseAttributeValue:true, ignoreAttributes:false})
+        // console.log('manifest:', manifest)
+        for (let i = 0; i < manifest.filelist.file.length; i++) {
+            let file = manifest.filelist.file[i]
+            // console.log(file['@_name'], file['@_size'], file['@_md5'], file['url'])
+            let pathNew = `./${argv.folderPath}/${file['@_name']}`
+            let p = path.parse(pathNew)
+            let downloadFile = true
+            if (fs.existsSync(pathNew)) {
+                downloadFile = !(await checkMd5(pathNew, file['@_md5'], () => {}))
+            }
+            try {fs.mkdirSync(p.dir, { recursive: true });} catch(e) {}
+            if (downloadFile) {
+                console.log(`Downloading file[${i}/${manifest.filelist.file.length}]: ${pathNew} ${(file['@_size']/1000000).toFixed(2)}MB`)
+                await download(file['url'], pathNew)
+                await checkMd5(pathNew, file['@_md5'])
+            } else {
+                console.log(`File already exists file[${i}/${manifest.filelist.file.length}]: ${pathNew} ${(file['@_size']/1000000).toFixed(2)}MB`)
+            }
+        }
+    })()
+}
+if (argv._[0] == 'gen' || argv._[0] == "generate") {
+    console.log("Generating manifest.")
+    // const stats = fs.statSync("myfile.txt");
+    // const fileSizeInBytes = stats.size;
+
+    const { domain, fromFolder, versionLabel, mirrors } = argv;
+    
+    (async () => {  
+        try {  
+            let files = await getFiles(fromFolder)
+            // console.log(files)
+            let fileList = await Promise.all(files.map(async f => {
+                let p = path.relative(`${process.cwd()}/${fromFolder}`, f);
+                // console.log(p);
+                if (p != '.DS_Store' && p != 'manifest.xml') {
+                    return {
+                        '@_name':p,
+                        '@_size':fs.statSync(f).size,
+                        url:[
+                            {'#text':`https://${domain}/${versionLabel}/${p}`},
+                            ...mirrors.map(d => ({
+                                '#text': `https://${d}/${versionLabel}/${p}`
+                            }))
+                        ],
+                        '@_md5': await checkMd5(f, undefined, () => {})
+                    }
+                }
+            }))
+            let m = {
+                manifest: {
+                    label:'City of Heroes Server',
+                    profiles:[{
+                        launch:[
+                            {
+                                '@_exec': "Ouroboros.exe",
+                                '@_order': "0",
+                                '@_params': `-project coh -patchDir ouro -uiskin 0 -auth ${domain}`,
+                                '@_website': `https://${domain}/public/`,
+                                '#text': "City of Heroes",
+                            },
+                            {
+                                '@_exec': "Ouroboros.exe",
+                                '@_order': "1",
+                                '@_params': `-project cov -patchDir ouro -uiskin 1 -auth ${domain}`,
+                                '@_website': `https://${domain}/public/`,
+                                '#text': "City of Villains",
+                            },
+                            {
+                                '@_exec': "Ouroboros.exe",
+                                '@_order': "2",
+                                '@_params': `-project gr -patchDir ouro -uiskin 2 -auth ${domain}`,
+                                '@_website': `https://${domain}/public/`,
+                                '#text': "Going Rogue",
+                            }
+                        ]
+                    }],
+                    filelist:{
+                        file:[...fileList]
+                    },
+                    forums:{
+                        forum:[
+                            { 
+                                '@_name':'Titan Network',
+                                '@_url':'https://www.cohtitan.com/forum/'
+                            }
+                        ]
+                    },
+                    launchers:{
+                        launcher:{
+                            '@_id':'CreamSoda',
+                            '@_size': 476672,
+                            '@_md5': 'b59e497bc48faee481fe3a7fe2fcd9be',
+                            '@_version': '1.01',
+                            url:{
+                                '#text': 'https://update.cityofheroesrebirth.com/CreamSoda.exe'
+                            }
+                        }
+                    },
+                    webpage: `https://${domain}/public/`,
+                    poster_image: {
+                        '@_url': `https://${domain}/public/images/ouroborosLogo.png`
+                    }
+                }
+            }
+            let toXML = new parser.j2xParser({parseAttributeValue:true, ignoreAttributes:false});
+            let data = toXML.parse(m)
+            fs.writeFileSync('manifest.xml', data, 'utf8')
+        } catch(e) {console.log(e)}
+    })()
+}
+
+async function getFiles(dir) {
+  const subdirs = fs.readdirSync(dir);
+  const files = await Promise.all(subdirs.map(async (subdir) => {
+    const res = path.resolve(dir, subdir);
+    return (fs.statSync(res)).isDirectory() ? getFiles(res) : res;
+  }));
+  return files.reduce((a, f) => a.concat(f), []);
+}
+
+var checkMd5 = function (pathNew, md5, print = console.log) {
+    return new Promise((res, rej) => {
+        let hash = crypto.createHash('md5')
+        const input = fs.createReadStream(pathNew);
+        input.on('readable', () => {
+            // Only one element is going to be produced by the
+            // hash stream.
+            const data = input.read();
+            if (data)
+                hash.update(data);
+            else {
+                let md5sum = hash.digest('hex')
+                print(`MD5 check: ${md5sum} ${(md5sum == md5) ? '✓' : '✗'}`);
+                res((md5) ? md5sum == md5 : md5sum);
+            }
+        });
+    })
+};
